10a0f3927d6b65e5bad2626b4f477faf2792b8e7
atulsingla82 <asingla@wgu.edu>
2017-10-12 13:12:22 -0400

    Done with styling

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+function change_text() {
+    var button = document.getElementById('toggle_button');
+
+    if (button.innerHTML == "facebook") {
+        button.innerHTML = "Atul";
+    }
+    else {
+        button.innerHTML == "facebook";
+    }
+}
+
